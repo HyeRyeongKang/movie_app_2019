dd468d9fadc8ad5d17d343caa7728bde4c07fa06
Ryeong <khr6604@gmail.com>
2019-11-12 17:27:56 +0900

    index edit

--- src/Movie.js
+++ src/Movie.js
@@ -5,18 +5,22 @@ import "./Movie.css";
 function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      <div className="movie_data">
+        <img src={poster} alt={title} title={title} />
+        <div className="movie_data_m">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="movie_data_s">
+        <p className="movie_summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
